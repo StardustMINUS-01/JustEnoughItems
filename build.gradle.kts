8a7ab0a0035bab704ededcb5d90b5a0e3e58eded
Tao <2584300846@qq.com>
2026-09-01 10:22:30 +0800

    chore: bump default build number to 183
    Prevents stale-jar confusion during field testing: the tested jar filename now
    unambiguously reflects the latest build (.183), old .181 jars removed from the
    deploy folder.

--- build.gradle.kts
+++ build.gradle.kts
@@ -71,7 +71,7 @@ subprojects {
     //adds the build number to the end of the version string if on a build server
     var buildNumber = project.findProperty("BUILD_NUMBER")
     if (buildNumber == null) {
-        buildNumber = "182"
+        buildNumber = "183"
     }
 
     version = "${specificationVersion}.${buildNumber}"
